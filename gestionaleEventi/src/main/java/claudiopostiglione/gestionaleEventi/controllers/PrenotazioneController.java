73296b197c253dde92912b772d588ef1b059a65c
Claudio9905 <cloudpost10@gmail.com>
2025-10-24 15:04:59 +0200

    adds file (Utente, Evento and Prenotazione - Controllers)

---
+++ gestionaleEventi/src/main/java/claudiopostiglione/gestionaleEventi/controllers/PrenotazioneController.java
@@ -0,0 +1,9 @@
+package claudiopostiglione.gestionaleEventi.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/prenotazioni")
+public class PrenotazioneController {
+}
