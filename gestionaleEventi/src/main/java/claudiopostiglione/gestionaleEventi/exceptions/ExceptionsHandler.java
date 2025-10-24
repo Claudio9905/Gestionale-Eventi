eee2377d561ea98bbc7938dbbc814d2a3b153902
Claudio9905 <cloudpost10@gmail.com>
2025-10-24 18:15:08 +0200

    Add new Exception Handler for status code: INTERNAL_SERVER_ERROR

--- gestionaleEventi/src/main/java/claudiopostiglione/gestionaleEventi/exceptions/ExceptionsHandler.java
+++ gestionaleEventi/src/main/java/claudiopostiglione/gestionaleEventi/exceptions/ExceptionsHandler.java
@@ -43,4 +43,11 @@ public class ExceptionsHandler extends RuntimeException {
         return new ErrorsWithListDTO(ex.getMessage(), LocalDate.now(), ex.getErrorsMessages());
     }
 
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ErrorsDTO handleException(Exception ex){
+        ex.printStackTrace();
+        return new ErrorsDTO(ex.getMessage(),LocalDate.now());
+    }
+
 }
